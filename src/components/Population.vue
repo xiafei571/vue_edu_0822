67ea25dd916d0c1251b837e1a3835d0d1b5cbe16
xiafei571 <xiafei571@gmail.com>
2020-08-23 02:32:20 +0900

    first commit

---
+++ src/components/Population.vue
@@ -0,0 +1,33 @@
+<template id="pop">
+    <div>
+        <p>{{result_data}}</p>
+
+    </div>
+
+</template>
+
+<script>
+import axios from "axios";
+
+const url = "http://localhost:8081/pop/info/1";
+
+export default {
+
+    data(){
+        return { result_data : "Hello world"}
+    },
+
+    mounted(){
+        axios.get(url).then(response => (this.result_data = response.data)).catch(function(error){
+            console.log(error)
+        })
+    }
+};
+
+
+
+</script>
+
+<style>
+
+</style>
